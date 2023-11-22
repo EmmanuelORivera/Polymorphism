0c3e56b8d3ad3843ed43abdca2ca4409320df765
Emmanuel Ochoa <eorivera95@gmail.com>
2023-11-21 23:56:28 -0600

    refactor: changing shape as an abstract class

--- Polymorphism/Shape.cs
+++ Polymorphism/Shape.cs
@@ -1,13 +1,16 @@
 ﻿namespace Polymorphism
 {
-	public class Shape
+	public abstract class Shape
 	{
+
+		// Rules
+		// 1. Abstact member can not include implementation.
+		// 2. If a member is declared as abstract, the containing class needs to be declared as abstract too.
+		// 3. Derived class must implement all abstract members in the base abstract class.
+		// 4. Abstract class can not be instantiated.
 		public int Width { get; set; }
 		public int Height { get; set; }
 
-		public virtual void Draw()
-		{
-            Console.WriteLine("Draw a shape");
-        }
+		public abstract void Draw();
 	}
 }
