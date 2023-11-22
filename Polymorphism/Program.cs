7c8cce8008099210c2a4a336c2ff29b1979ad4b2
Emmanuel Ochoa <eorivera95@gmail.com>
2023-11-22 17:07:36 -0600

    feat: database connection example

--- Polymorphism/Program.cs
+++ Polymorphism/Program.cs
@@ -2,11 +2,66 @@
 
 namespace Polymorphism
 {
+	public abstract class DbConnection
+	{
+		protected string ConnectionString { get; set; }
+		protected TimeSpan Timeout { get; set; }
+
+		public DbConnection(string connectionString)
+		{
+			if (string.IsNullOrEmpty(connectionString)) throw new ArgumentException("ConnectionString can't be an empty string or null");
+			ConnectionString = connectionString;
+		}
+
+		public void SetTimeout(TimeSpan timeout)
+		{
+			if (timeout < TimeSpan.Zero) throw new ArgumentException("Timeout cannot be negative time span");
+
+			Timeout = timeout;
+		}
+
+		public abstract void Open();
+		public abstract void Close();
+	}
+
+	public class SqlConnection : DbConnection
+	{
+		public SqlConnection(string connectionString) : base(connectionString)
+		{
+		}
+
+		public override void Close()
+		{
+			Console.WriteLine("Close SqlConnection");
+		}
+
+		public override void Open()
+		{
+			Console.WriteLine("Open SqlConnection");
+		}
+	}
+	public class OracleConnection : DbConnection
+	{
+		public OracleConnection(string connectionString) : base(connectionString)
+		{
+		}
+
+		public override void Close()
+		{
+			Console.WriteLine("Close OracleConnection");
+		}
+
+		public override void Open()
+		{
+			Console.WriteLine("Open OracleConnection");
+		}
+	}
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
 			UseDrawShapes();
+			UseDatabaseConnection();
 		}
 		static void UseDrawShapes()
 		{
@@ -17,6 +72,16 @@ namespace Polymorphism
 
 			var canvas = new Canvas();
 			canvas.DrawShapes(shapes);
+		}
+
+		static void UseDatabaseConnection()
+		{
+			var sqlConnection = new SqlConnection("This is a sql connectionString");
+			sqlConnection.Open();
+			sqlConnection.Close();
+			var oracleConnection = new OracleConnection("This is a oracle connectionString");
+			oracleConnection.Open();
+			oracleConnection.Close();	
 		}
 	}
 }
